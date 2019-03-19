ef86dc653a0a05fa0882a50ab7293b565f2d3d5b
amjad <amjadals@maxis.com.my>
2019-03-19 10:18:41 +0800

    init-project

---
+++ front-end/src/Board.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import Note from './Note'
+// import FaPlus from 'react-icons/lib/fa/plus'
+
+class Board extends Component {
+
+
+
+	render() {
+		return (
+			<div className="board grid">
+ 
+			</div>
+		)
+	}
+}
+
+export default Board
