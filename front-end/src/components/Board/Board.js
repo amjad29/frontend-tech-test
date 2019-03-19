305d35a0c1988cc6c71431fc64d53243eaa172f8
amjad <amjadals@maxis.com.my>
2019-03-20 02:24:22 +0800

    note-drag-n-drop-edit-delete

--- front-end/src/components/Board/Board.js
+++ front-end/src/components/Board/Board.js
@@ -10,10 +10,12 @@ class Board extends Component {
             notes: []
         }
         this.add = this.add.bind(this)
-        // this.eachNote = this.eachNote.bind(this)
-        // this.update = this.update.bind(this)
-        // this.remove = this.remove.bind(this)
-        // this.nextId = this.nextId.bind(this)
+        this.drop = this.drop.bind(this)
+        this.allowDrop = this.allowDrop.bind(this)
+        this.eachNote = this.eachNote.bind(this)
+        this.update = this.update.bind(this)
+        this.remove = this.remove.bind(this)
+        this.nextId = this.nextId.bind(this)
     }
 
     add(text) {
@@ -33,20 +35,50 @@ class Board extends Component {
         return this.uniqueId++
     }
 
+    update(newText, i) {
+        console.log('updating item at index', i, newText)
+        this.setState(prevState => ({
+            notes: prevState.notes.map(
+                note => (note.id !== i) ? note : { ...note, note: newText }
+            )
+        }))
+    }
+
+    remove(id) {
+      
+        this.setState(prevState => ({
+            notes: prevState.notes.filter(note => note.id !== id)
+        }))
+    }
+
     eachNote(note, i) {
+        console.log('note==>',note)
         return (
+        
             <Note key={note.id}
-                index={note.id}>
+                index={note.id}
+                onChange={this.update}
+                onRemove={this.remove}>
                 {note.note}
             </Note>
         )
     }
 
+    drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
+
+    allowDrop(ev) {
+        ev.preventDefault();
+    }
+
 
     render() {
         return (
-            <div className="board grid-x">
-                <div class="flex-container">
+            <div className="board" >
+                <div class="flex-container" onDrop={this.drop} onDragOver={this.allowDrop}>
 
                     {this.state.notes.map(this.eachNote)}
                     <button id="add" onClick={this.add.bind(null, "New Note")}>
--- front-end/src/components/Note/Note.js
+++ front-end/src/components/Note/Note.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import Modal from 'react-responsive-modal';
 import './style.scss';
-// import Note from './Note'
-// import FaPlus from 'react-icons/lib/fa/plus'
 
 class Note extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            editing: false,
+            open: false
+        }
+
+        this.edit = this.edit.bind(this)
+        this.drag = this.drag.bind(this)
+        this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+        // this.randomBetween = this.randomBetween.bind(this)
+    }
+
+    onOpenModal = () => {
+        this.setState({ open: true });
+    };
+
+    onCloseModal = () => {
+        this.setState({ open: false });
+    };
 
     randomBetween(x, y, s) {
         return x + Math.ceil(Math.random() * (y - x)) + s
@@ -17,16 +39,68 @@ class Note extends Component {
         }
     }
 
+    edit() {
+        this.setState({
+            editing: true
+        })
+    }
+
+    remove() {
+        this.props.onRemove(this.props.index)
+    }
+    
+    drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+        console.log(' ev.dataTransfer==>', ev.dataTransfer)
+        console.log(' ev.target.id==>', ev.target.id)
+    }
+
+    save(e) {
+        e.preventDefault()
+        this.props.onChange(this._newText.value, this.props.index)
+        this.setState({
+            editing: false
+        })
+    }
+
+
+    renderForm() {
+        return (
+            <div className="note" style={this.style}>
+                <form onSubmit={this.save}>
+                    <textarea ref={input => this._newText = input}
+                        defaultValue={this.props.children} />
+                    <button id="save">save</button>
+                </form>
+            </div>
+        )
+    }
+
+    renderDisplay() {
+        const { open } = this.state;
+        console.log('props==>',this.props)
+        console.log('props.key==>',this.props.index)
+        return (
+            <div className="note" draggable="true" id={this.props.index}  onDragStart={this.drag} style={this.style}>
+                <p>{this.props.children}</p>
+                <span>
+                    {/* <button onClick={this.edit} id="edit">edit</button> */}
+                    <button onClick={this.onOpenModal} id="edit">edit</button>
+                    <button onClick={this.remove} id="remove">delete</button>
+                </span>
+                <Modal open={open} onClose={this.onCloseModal} center>
+                    <form onSubmit={this.save}>
+                        <textarea ref={input => this._newText = input}
+                            defaultValue={this.props.children} />
+                        <button id="save">save</button>
+                    </form>
+                </Modal>
+            </div>
+        )
+    }
 
     render() {
-        return (
-
-            <div className="note" style={this.style}>
-                <p></p>
-                <span> test</span>
-            </div>
-
-        )
+        return this.state.editing ? this.renderForm() : this.renderDisplay()
     }
 }
 
